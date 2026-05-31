8120607e3348c1995e46a4b99dfd4b92620fb959
Marc Haisenko <haisenko@equinux.com>
2026-05-31 14:46:39 +0200

    Add some more statistics

--- Sources/UI/Statistics/StatisticsView.swift
+++ Sources/UI/Statistics/StatisticsView.swift
@@ -19,24 +19,17 @@ struct StatisticsView: View {
     @Environment(\.colorScheme) private
     var colorScheme
     
+    @Environment(\.verticalSizeClass) private
+    var verticalSizeClass
+    
     
     var body: some View {
         let statistics = Statistics.derive(from: performances, colorScheme: colorScheme)
         
-        VStack {
-            Grid(alignment: .leading) {
-                GridRow {
-                    Text("Bands:")
-                    Text(String(statistics.numberOfBands))
-                }
-                GridRow {
-                    Text("Venues:")
-                    Text(String(statistics.numberOfVenues))
-                }
-                GridRow {
-                    Text("Performances:")
-                    Text(String(statistics.numberOfPerformances))
-                }
+        VStack(alignment: .leading) {
+            
+            if verticalSizeClass != .compact {
+                textStatistics(statistics)
             }
             
             Chart {
@@ -71,6 +64,59 @@ extension StatisticsView {
         let numberOfPerformances: Int
         
         let numberOfPerformancesPerYear: [(year: Int, count: Int, color: Color)]
+        
+        let top10Bands: [(band: String, count: Int, rank: Int?)]
+    }
+    
+    
+    @ViewBuilder
+    func textStatistics(_ statistics: Statistics) -> some View {
+        Grid(alignment: .leading) {
+            GridRow {
+                Text(String()) // To avoid getting a `""` in the Localizable.xcstrings
+                Text("Bands:")
+                Text(String(statistics.numberOfBands))
+            }
+            GridRow {
+                Text(String()) // To avoid getting a `""` in the Localizable.xcstrings
+                Text("Venues:")
+                Text(String(statistics.numberOfVenues))
+            }
+            GridRow {
+                Text(String()) // To avoid getting a `""` in the Localizable.xcstrings
+                Text("Performances:")
+                Text(String(statistics.numberOfPerformances))
+            }
+            
+            Divider()
+                .gridCellColumns(4)
+            
+            GridRow {
+                Text("Top 10 bands:")
+                    .font(.headline)
+                    .padding(.bottom, 4)
+                    .gridCellColumns(2)
+            }
+            
+            let top10Bands = statistics.top10Bands
+            ForEach(top10Bands.indices, id: \.self) {
+                (index) in
+                
+                GridRow {
+                    if let rank = top10Bands[index].rank {
+                        Text(String(rank) + ".")
+                            .monospacedDigit()
+                            .gridColumnAlignment(.trailing)
+                    } else {
+                        Text(String()) // To avoid getting a `""` in the Localizable.xcstrings
+                    }
+                    
+                    Text(top10Bands[index].band + ":")
+                    
+                    Text(String(top10Bands[index].count))
+                }
+            }
+        }
     }
     
 }
@@ -84,7 +130,8 @@ extension StatisticsView.Statistics {
             numberOfBands: 0,
             numberOfVenues: 0,
             numberOfPerformances: 0,
-            numberOfPerformancesPerYear: []
+            numberOfPerformancesPerYear: [],
+            top10Bands: [],
         )
     }
     
@@ -92,12 +139,17 @@ extension StatisticsView.Statistics {
         var bands: Set<String> = []
         var venues: Set<String> = []
         var numberOfPerformances = 0
-        
         var numberOfPerformancesPerYear: [Int: Int] = [:]
+        var bandsCount: [String: Int] = [:]
+        
         
         for performance in performances where !performance.partialAttendance {
             if let bandName = performance.band?.name {
                 bands.insert(bandName)
+                
+                if !performance.partialAttendance {
+                    bandsCount[bandName, default: 0] += 1
+                }
             }
             if let venueName = performance.venue?.name {
                 venues.insert(venueName)
@@ -108,6 +160,22 @@ extension StatisticsView.Statistics {
             numberOfPerformancesPerYear[year, default: 0] += 1
         }
         
+        return Self(
+            numberOfBands: bands.count,
+            numberOfVenues: venues.count,
+            numberOfPerformances: performances.count,
+            numberOfPerformancesPerYear: derivePerformancesPerYear(
+                numberOfPerformancesPerYear,
+                colorScheme: colorScheme
+            ),
+            top10Bands: deriveRankedBands(bandsCount)
+        )
+    }
+    
+    static func derivePerformancesPerYear(
+        _ numberOfPerformancesPerYear: [Int: Int],
+        colorScheme: ColorScheme
+    ) -> [(year: Int, count: Int, color: Color)] {
         let colors = DiscreteColorGradient.colors(
             from: .green,
             to: .blue,
@@ -121,12 +189,35 @@ extension StatisticsView.Statistics {
             performancesPerYear.append((year: year, count: count, color: colors[index]))
         }
         
-        return Self(
-            numberOfBands: bands.count,
-            numberOfVenues: venues.count,
-            numberOfPerformances: performances.count,
-            numberOfPerformancesPerYear: performancesPerYear
-        )
+        return performancesPerYear
+    }
+    
+    static func deriveRankedBands(_ bandsCount: [String: Int]) -> [(band: String, count: Int, rank: Int?)] {
+        let topBands = bandsCount.sorted(by: {
+            if $0.value != $1.value {
+                return $0.value > $1.value
+            }
+            
+            if $0.key.localizedStandardCompare($1.key) == .orderedAscending {
+                return true
+            }
+            
+            return false
+        })
+            .prefix(10)
+        
+        var rankedBands: [(String, Int, Int?)] = []
+        var shows = 0
+        for (index, element) in topBands.enumerated() {
+            if shows != element.value {
+                rankedBands.append((element.key, element.value, index + 1))
+                shows = element.value
+            } else {
+                rankedBands.append((element.key, element.value, nil))
+            }
+        }
+        
+        return rankedBands
     }
     
 }
